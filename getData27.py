edf1ce4304ac46775e3d5f171e60a220ae2cbfbc
Wictor <wictorsmartins@gmail.com>
2017-07-25 19:54:58 -0300

    Coloque inputs de data incial e data final, input de candlestick

--- getData27.py
+++ getData27.py
@@ -1,26 +1,22 @@
 import os
 import time
 import timestamp
+import shutil
 import pandas as pd
 
-FETCH_URL = "https://poloniex.com/public?command=returnChartData&currencyPair=%s&start=%d&end=%d&period=300"
+FETCH_URL = "https://poloniex.com/public?command=returnChartData&currencyPair=%s&start=%d&end=%d&period=%d"
 #PAIR_LIST = ["BTC_ETH"]
 DATA_DIR = "data"
 COLUMNS = ["date","high","low","open","close","volume","quoteVolume","weightedAverage"]
 
-def get_data(pair):
+def get_data(pair, start_time, end_time, candle):
+
     datafile = os.path.join(DATA_DIR, pair+".csv")
     timefile = os.path.join(DATA_DIR, pair)
 
     newfile = True
-    start_time = 1420070400
-    end_time = 1500940800
-    url = FETCH_URL % (pair, start_time, end_time)
 
-    start = timestamp.timeConvert(start_time)
-    end = timestamp.timeConvert(end_time)
-
-    print("Get %s from %s to %s" % (pair, start, end))
+    url = FETCH_URL % (pair, start_time, end_time, candle)
 
     df = pd.read_json(url)
 
@@ -41,21 +37,31 @@ def get_data(pair):
         df.to_csv(outf, index=False, columns=COLUMNS, header=False)
     outf.close()
     print("Finish.")
-    time.sleep(30)
-
+    time.sleep(3)
 
 
 def main():
-    if not os.path.exists(DATA_DIR):
-        os.mkdir(DATA_DIR)
 
-    df = pd.read_json("https://poloniex.com/public?command=return24hVolume")
-    pairs = [pair for pair in df.columns if pair.startswith('USDT_BTC')]
-    print(pairs)
+    start_time = timestamp.dateToTimestampStart()
+    end_time = timestamp.dateToTimestampEnd()
+    print("Valores do Candlestick")
+    print("""
+    5 min = 300 
+    15 min = 900  
+    30 min = 1800  
+    2 hr = 7200
+    4 hr = 14400  
+    1 dia = 86400""")
+    candle = input("Candlestick: ")
+    if os.path.exists(DATA_DIR):
+        shutil.rmtree(DATA_DIR)
 
-    for pair in pairs:
-        get_data(pair)
-        time.sleep(2)
+    os.mkdir(DATA_DIR)
+
+    pair = "USDT_BTC"
+    print(pair)
+    get_data(pair, start_time, end_time, candle)
+    time.sleep(6)
 
 if __name__ == '__main__':
     main()
--- timestamp.py
+++ timestamp.py
@@ -1,6 +1,22 @@
-from datetime import datetime
+import time
+import datetime
 
-def timeConvert(unix_timestamp):
-    utc_time = datetime.utcfromtimestamp(unix_timestamp)
- #   return utc_time.strftime("%Y-%m-%d %H:%M:%S.%f+00:00 (UTC)")
+
+def timeConvert(start_time):
+
+    utc_time = datetime.utcfromtimestamp(start_time)
+#   return utc_time.strftime("%Y-%m-%d %H:%M:%S.%f+00:00 (UTC)")
     return utc_time.strftime("%Y-%m-%d %H:%M:%S")
+
+def dateToTimestampStart():
+
+    data = raw_input("Data inicial - Ex: 01-12-2017: ")
+    timestamp = time.mktime(time.strptime(data, '%d-%m-%Y'))
+    return int(timestamp)
+
+
+def dateToTimestampEnd():
+
+    data =raw_input("Data final - Ex: 01-12-2017: ")
+    timestamp = time.mktime(time.strptime(data, '%d-%m-%Y'))
+    return int(timestamp)
